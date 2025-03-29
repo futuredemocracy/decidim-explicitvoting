38fad8b22c57006549d8059fb51c40cb1d3d394e
Rafal Styblinski <rafal.styblinski@gmail.com>
2025-03-29 20:13:38 +0100

    fix - already can create vote but there are issues

--- decidim-explicit-voting/lib/decidim/explicit_voting/engine.rb
+++ decidim-explicit-voting/lib/decidim/explicit_voting/engine.rb
@@ -2,6 +2,7 @@
 
 module Decidim
   module ExplicitVoting
+    # This is the engine that runs on the public interface of explicit_voting.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::ExplicitVoting
 
@@ -13,8 +14,34 @@ module Decidim
         root to: "votings#index"
       end
 
+      initializer "decidim_explicit_voting.icons" do
+        Decidim.icons.register(name: "pencil", icon: "pencil-alt", category: "system", description: "Edit icon", engine: :admin)
+        Decidim.icons.register(name: "eye", icon: "eye", category: "system", description: "View icon", engine: :admin)
+        Decidim.icons.register(name: "trash", icon: "trash", category: "system", description: "Delete icon", engine: :admin)
+        Decidim.icons.register(name: "check", icon: "check", category: "system", description: "Check icon", engine: :admin)
+        Decidim.icons.register(name: "download", icon: "download", category: "system", description: "Download icon", engine: :admin)
+        Decidim.icons.register(name: "clipboard-list", icon: "clipboard-list", category: "system", description: "Results icon", engine: :admin)
+      end
+
       initializer "decidim_explicit_voting.add_cells_view_paths" do
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::ExplicitVoting::Engine.root}/app/cells")
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::ExplicitVoting::Engine.root}/app/views") # for partials
+      end
+
+      initializer "decidim_explicit_voting.mount_routes" do
+        Decidim::Core::Engine.routes do
+          mount Decidim::ExplicitVoting::Engine, at: "/", as: "decidim_explicit_voting"
+        end
+      end
+
+      initializer "decidim_explicit_voting.add_settings" do
+        Decidim.component_registry.find(:explicit_voting).settings(:global) do |settings|
+          settings.attribute :announcement, type: :text, translated: true, editor: true
+        end
+
+        Decidim.component_registry.find(:explicit_voting).settings(:step) do |settings|
+          settings.attribute :announcement, type: :text, translated: true, editor: true
+        end
       end
 
       initializer "decidim_explicit_voting.webpacker.assets_path" do
