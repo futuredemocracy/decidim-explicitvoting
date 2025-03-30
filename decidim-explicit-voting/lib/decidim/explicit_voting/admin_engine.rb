e40750d37a557923655e0c5126b18289dbaef73b
Rafal Styblinski <rafal.styblinski@gmail.com>
2025-03-30 15:28:43 +0200

    Further fix and icon registration. Administration works again

--- decidim-explicit-voting/lib/decidim/explicit_voting/admin_engine.rb
+++ decidim-explicit-voting/lib/decidim/explicit_voting/admin_engine.rb
@@ -34,6 +34,10 @@ module Decidim
         Cell::ViewModel.view_paths << File.expand_path("#{Decidim::ExplicitVoting::Engine.root}/app/cells")
       end
 
+      initializer "decidim_explicit_voting_admin.register_icons" do
+        Decidim.icons.register(name: "chart-bar", icon: "chart-bar", category: "system", description: "Chart icon", engine: :core)
+      end
+
       def load_seed
         nil
       end
